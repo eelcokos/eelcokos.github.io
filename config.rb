36af5aa34a19132e3a451f91501d6a9ae2ab7aa0
eelcokos <eelcokos@hotmail.com>
2015-10-20 22:34:16 +0200

    Path update

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require "susy"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/scss"
-images_dir = "assets/img"
-javascripts_dir = "assets/js"
+css_dir = "_assets/css"
+sass_dir = "_assets/scss"
+images_dir = "_assets/img"
+javascripts_dir = "_assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
